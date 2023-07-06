5a52ed7c7dabd2e49faa1aebfc4393f8547db5c8
Michelle Wang <tomichellewang@gmail.com>
2023-07-06 16:44:35 -0400

    add heuristic and update BIDS conversion script

--- workflow/bids_conv/heuristic.py
+++ workflow/bids_conv/heuristic.py
@@ -1,182 +1,218 @@
-# -*- coding: utf-8 -*-
-"""
-Heuristics created by Vincent for PPMI dataset T1 images.
-created @ 22th Mar. 2022
-Updated by nikhil153 @ 25 Feb 2023
-"""
-import os
-import logging
+import json
+import re
+from collections import defaultdict
+from pathlib import Path
+from typing import Sequence
 
-lgr = logging.getLogger(__name__)
-scaninfo_suffix = '.json'
+import pandas as pd # part of Heudiconv container
 
-# scanning protocol details
-T1W_SERIES = [
-    # added by nikhi153
-    'SAG 3D MPR',
-    'MPRAGE 2 ADNI',
-    'MPRAGE ADNI',
-    'MPRAGE GRAPPA 2',
-    'MPRAGE GRAPPA2',
-    'MPRAGE GRAPPA2(adni)',
-    'MPRAGE w/ GRAPPA',
-    'MPRAGE_GRAPPA',
-    'MPRAGE_GRAPPA_ADNI',
-    'MPRAGE GRAPPA',
-    'SAG T1 3D MPRAGE',
-    'sag mprage',
-    'MPRAGEadni',
-    'MPRAGE GRAPPA_ND',
-    '3D SAG',
-    'MPRAGE T1 SAG',
-    'MPRAGE SAG',
-    'SAG T1 3DMPRAGE',
-    'SAG T1 MPRAGE',
-    'SAG 3D T1',
-    'SAG MPRAGE GRAPPA2-NEW2016',
-    'SAG MPRAGE GRAPPA_ND',
-    'Sag MPRAGE GRAPPA',
-    'AXIAL T1 3D MPRAGE',
-    'SAG MPRAGE GRAPPA',
-    'sT1W_3D_FFE',
-    'sT1W_3D_ISO',
-    'sT1W_3D_TFE',
-    'sag 3D FSPGR BRAVO straight',
-    'SAG T1 3D FSPGR',
-    'SAG FSPGR 3D '
-    'SAG 3D FSPGR BRAVO STRAIGHT',
-    'SAG T1 3D FSPGR 3RD REPEAT',
-    'SAG FSPGR BRAVO',
-    'SAG SPGR 3D',
-    'SAG 3D SPGR',
-    'FSPGR 3D SAG',
-    'SAG FSPGR 3D',
-    'SAG 3D FSPGR BRAVO STRAIGHT',
-    'SAG FSPGR 3D ',
-    't1_mpr_ns_sag_p2_iso',
-    'T1',
-    'T1 Repeat',
-    'AX T1',
-    'axial spgr',
-    'T1W_3D_FFE AX',
-    # added by Vincent
-    'AX T1 SE C+',
-    '3D SAG T1 MPRAGE',
-    '3D SAG T1 MPRAGE_ND',
-    '3D T1',
-    '3D T1 MPRAGE',
-    '3D T1-weighted',
-    'Accelerated Sag IR-FSPGR',
-    'MPRAGE',
-    'MPRAGE - Sag',
-    'MPRAGE Phantom GRAPPA2',
-    'MPRAGE w/ GRAPPA 2',
-    'PPMI_MPRAGE_GRAPPA2',
-    'SAG 3D T1 FSPGR',
-    'SAG FSPGR 3D VOLUMETRIC T1',
-    'Sag MPRAGE GRAPPA_ND',
-    'T1-weighted, 3D VOLUMETRIC',
-    'tra_T1_MPRAGE', 
-    '3D T1-weighted_ND', ## added from livingpark
-    '3D T1 _weighted',
-    'Sagittal 3D Accelerated MPRAGE',
-    'T1 REPEAT',
-    'MPRAGE Repeat',
-    'SAG_3D_MPRAGE',
-    'T1-weighted,_3D_VOLUMETRIC', ## added from all T1 PPMI
-    '3D_SAG',
-    'FSPGR_3D_SAG',
-    'SAG_T1_MPRAGE',
-    'MPRAGE_2_ADNI',
-    'MPRAGE_Repeat',
-    'T1W_3D_FFE_COR',
-    'SAG',
-    'SAG_MPRAGE_GRAPPA2-NEW2016',
-    'MPRAGE_GRAPPA_2',
-    'AX_T1_SE_C+',
-    '3D_Sagittal_T1',
-    'Coronal',
-    'MPRAGE_SENSE2',
-    'sag_mprage',
-    'Accelerated_Sag_IR-FSPGR',
-    'SAG_SPGR',
-    'MPRAGE_GRAPPA2_adni_',
-    'Sag_MPRAGE_GRAPPA',
-    '3D_SAG_T1_MPRAGE',
-    'MPRAGE_ADNI',
-    'AX_3D_FSPGR_straight_brain_lab',
-    '3D_T1_MPRAGE',
-    'rpt_PPMI_MPRAGE_GRAPPA2',
-    'SAG_FSPGR_BRAVO',
-    '3D_T1',
-    'sag_3D_FSPGR_BRAVO_straight',
-    'mprage',
-    'AX_T1',
-    'Sagittal_3D_Accelerated_MPRAGE',
-    'MPRAGE_w__GRAPPA',
-    'SAG_T1_3D_FSPGR',
-    'SAG_3D_MPRAGE_RPT',
-    'SAG_T1_SE',
-    'axial_spgr',
-    'SAG_T1_3D_FSPGR_3RD_REPEAT',
-    'SAG_FSPGR_3D',
-    'MPRAGE_SAG',
-    'T1_repeat',
-    'T1_SAG',
-    'Sag_T1',
-    'T1_REPEAT',
-    'T1W_3D_FFE_AX',
-    '3D_T1-weighted',
-    'SAG_T1_3DMPRAGE',
-    'MPRAGE_T1_SAG',
-    'SAG_FSPGR_3D_VOLUMETRIC_T1',
-    'SAG_3D_T1_FSPGR',
-    'SAG_3D_T1',
-    'T1_Repeat',
-    'SAG_SPGR_3D',
-    'MPRAGE_GRAPPA2', 
-    'MPRAGE_-_Sag',
-    'SAG_3D_FSPGR_BRAVO_STRAIGHT',
-    'SAG_MPRAGE_GRAPPA',
-]
+# BIDS standard
+DATATYPE_ANAT = 'anat'
+DATATYPE_DWI = 'dwi'
+SUFFIX_T1 = 'T1w'
+SUFFIX_T2 = 'T2w'
+SUFFIX_T2_STAR = 'T2starw'
+SUFFIX_FLAIR = 'FLAIR'
 
-def create_key(template, outtype=('nii.gz',), annotation_classes=None):
-    if template is None or not template:
-        raise ValueError('Template must be a valid format string')
-    return template, outtype, annotation_classes
+# datatype/suffix to description mapping (from mr_proc-ppmi script)
+FPATH_DESCRIPTIONS = Path('/scratch/proc/ppmi_imaging_descriptions.json')
+
+# LONI IDA Search result file
+FPATH_IMAGING = Path('/scratch/tabular/other/idaSearch.csv') # TODO update when this file gets moved
+COL_PROTOCOL = 'Imaging Protocol'
+COL_IMAGE_ID = 'Image ID'
+RE_IMAGE_ID = '.*_I([0-9]+).dcm' # regex
+RE_NEUROMELANIN = '[nN][mM]' # neuromelanin pattern
+SEP_PROTOCOL_INFO_ENTRY = ';'
+SEP_PROTOCOL_INFO = '='
+KEY_PLANE = 'Acquisition Plane'
+KEY_DIMS = 'Acquisition Type'
+
+# dwi directions
+VALID_DIRS = ['AP', 'PA', 'LR', 'RL']
+DIR_RE_MAP = {
+    dir: f'[ \-_]{dir[0]}[ \-_>]*{dir[1]}(?:[ \-_]|\Z)'
+    for dir in VALID_DIRS
+}
+DIR_DESCRIPTIONS_MAP = {
+    'LR': [
+        '2D DTI EPI FAT SHIFT LEFT',
+        'AX DTI 32 DIR FAT SHIFT L',
+        'AX DTI 32 DIR FAT SHIFT L NO ANGLE',
+        'AX DTI _reverse', # one subject has this and 'AX DTI _RL'
+
+    ],
+    'RL': [
+        '2D DTI EPI FAT SHIFT RIGHT',
+        'AX DTI 32 DIR FAT SHIFT R',
+        'AX DTI 32 DIR FAT SHIFT R NO ANGLE'
+    ]
+}
+
+TAG_NEUROMELANIN = 'NM'
+TAG_SAG = 'sag'
+TAG_COR = 'cor'
+TAG_AX = 'ax'
+TAG_2D = '2D'
+TAG_3D = '3D'
+MAP_PLANE = {'SAGITTAL': TAG_SAG, 'CORONAL': TAG_COR, 'AXIAL': TAG_AX}
+MAP_DIMS = {'2D': TAG_2D, '3D': TAG_3D}
+PATTERN_ITEM = '{item:02d}'
+
+HEURISTIC_HELPER = None
 
 def infotodict(seqinfo):
     """Heuristic evaluator for determining which runs belong where
     allowed template fields - follow python string module:
     item: index within category
     subject: participant id
-    seqitem: run number during scanning
-    subindex: sub index within group
+    session: session id (included 'ses-' prefix)
     """
-    t1w = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_run-{item:02d}_T1w')  # noqa
-    t1w_grappa = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-grappa2_run-{item:02d}_T1w')  # noqa
-    t1w_adni   = create_key('sub-{subject}/{session}/anat/sub-{subject}_{session}_acq-adni_run-{item:02d}_T1w')  # noqa
-    ####
-    info = {t1w: [], t1w_grappa: [], t1w_adni: []}
-    revlookup = {}
+    
+    global HEURISTIC_HELPER
 
-    for idx, s in enumerate(seqinfo):
-        revlookup[s.series_id] = s.series_description
-        print(s) 
-        if s.series_description in T1W_SERIES:# T1
-            info[t1w].append(s.series_id)
+    if HEURISTIC_HELPER is None:
+        HEURISTIC_HELPER = HeuristicHelper()
 
-    # Adding "acq" for all t1w
-    if len(info[t1w]) > 1:
-        # copy out t1w image series ids and reset info[t1w]
-        all_t1w = info[t1w].copy()
-        info[t1w] = []
-        for series_id in all_t1w:
-            series_description = revlookup[series_id].lower()
-            if series_description in ['mprage_grappa', 'sag_mprage_grappa']:
-                info[t1w].append(series_id)
-            elif 'adni' in series_description:
-                info[t1w_adni].append(series_id)
+    info = defaultdict(list)
+    for _, s in enumerate(seqinfo):
+        
+        image_id = get_image_id_from_dcm(s.example_dcm_file)
+        try:
+            datatype, suffix = HEURISTIC_HELPER.get_datatype_suffix_from_description(s.series_description)
+        except TypeError:
+            raise RuntimeError(f'Did not find description {s.series_description} in mapping for datatypes {HEURISTIC_HELPER.datatypes} for image {image_id}')
+
+        imaging_protocol_info = {}
+        for protocol_info_entry in HEURISTIC_HELPER.df_imaging.loc[image_id, COL_PROTOCOL].split(SEP_PROTOCOL_INFO_ENTRY):
+            key, value = protocol_info_entry.split(SEP_PROTOCOL_INFO)
+            imaging_protocol_info[key] = value
+
+        plane = None # to fill in
+        dims = None
+        if datatype == DATATYPE_ANAT:
+
+            if re.search(RE_NEUROMELANIN, s.series_description):
+                info[create_key_anat(suffix, acq=TAG_NEUROMELANIN)].append(s.series_id)
+
             else:
-                info[t1w_grappa].append(series_id)
+
+                # image dimensions: 2D or 3D
+                try:
+                    dims = MAP_DIMS[imaging_protocol_info[KEY_DIMS]]
+                except KeyError:
+                    pass
+
+                # acquisition plane: sagittal, coronal, or axial
+                try:
+                    plane = MAP_PLANE[imaging_protocol_info[KEY_PLANE]]
+                except KeyError:
+                    pass
+
+                info[create_key_anat(suffix, plane=plane, dims=dims)].append(s.series_id)
+
+        elif datatype == DATATYPE_DWI:
+            dir = get_dwi_direction_from_description(s.series_description)
+            info[create_key_dwi('dwi', dir=dir)].append(s.series_id)
+
+        else:
+            raise NotImplementedError(f'Not implemented for datatype {datatype} yet')
+
     return info
+
+def create_key_anat(suffix, plane=None, dims=None, acq=None):
+
+    if (acq is not None) and (plane is not None or dims is not None):
+        raise RuntimeError('Cannot specify both acq and plane/dims')
+    
+    if (plane is not None) and (dims is not None):
+        stem = f'sub-{{subject}}_{{session}}_acq-{plane}{dims}_run-{PATTERN_ITEM}_{suffix}'
+    elif acq is not None:
+        stem = f'sub-{{subject}}_{{session}}_acq-{acq}_run-{PATTERN_ITEM}_{suffix}'
+    else:
+        stem = f'sub-{{subject}}_{{session}}_run-{PATTERN_ITEM}_{suffix}'
+
+    return create_key(DATATYPE_ANAT, stem)
+
+def create_key_dwi(suffix, dir=None):
+    if dir is None:
+        stem = f'sub-{{subject}}_{{session}}_run-{PATTERN_ITEM}_{suffix}'
+    else:
+        stem = f'sub-{{subject}}_{{session}}_dir-{dir}_run-{PATTERN_ITEM}_{suffix}'
+    return create_key(DATATYPE_DWI, stem)
+
+def create_key(datatype, stem, outtype=('nii.gz',), annotation_classes=None):
+    template = f'sub-{{subject}}/{{session}}/{datatype}/{stem}'
+    return template, outtype, annotation_classes
+
+def get_image_id_from_dcm(fname_dcm):
+    match = re.match(RE_IMAGE_ID, fname_dcm)
+    if not match:
+        raise RuntimeError(f'Could not get image ID from {fname_dcm}')
+    if len(match.groups()) > 1:
+        raise RuntimeError(f'Got more than one image ID from {fname_dcm}')
+    return match.group(1)
+
+def get_dwi_direction_from_description(description: str):
+
+    # check hardcoded description strings first
+    for dir, descriptions in DIR_DESCRIPTIONS_MAP.items():
+        if description in descriptions:
+            return dir
+
+    # then infer using regexes
+    for dir, re_dir in DIR_RE_MAP.items():
+        if re.search(re_dir, description):
+            return dir
+        
+    # no direction found
+    return None
+
+class HeuristicHelper:
+
+    datatypes = [DATATYPE_ANAT, DATATYPE_DWI]
+    suffixes_anat = [SUFFIX_T1, SUFFIX_T2, SUFFIX_T2_STAR, SUFFIX_FLAIR]
+
+    def __init__(self) -> None:
+
+        self.fpath_imaging = FPATH_IMAGING
+        self.fpath_descriptions = FPATH_DESCRIPTIONS
+
+        if not self.fpath_imaging.exists():
+            raise FileNotFoundError(f'Imaging info file {self.fpath_imaging} does not exist')
+        
+        if not self.fpath_descriptions.exists():
+            raise FileNotFoundError(f'Descriptions map file {self.fpath_descriptions} does not exist')
+        
+        self.df_imaging = pd.read_csv(self.fpath_imaging, dtype=str).set_index(COL_IMAGE_ID)
+        
+        with open(self.fpath_descriptions, 'r') as file_descriptions:
+            self.descriptions_map = json.load(file_descriptions)
+
+    def get_descriptions(self, keys) -> list[str]:
+
+        keys_all = keys[:]
+
+        descriptions = self.descriptions_map
+        while len(keys) > 0:
+            key = keys.pop(0)
+            try:
+                descriptions = descriptions[key]
+            except KeyError:
+                raise KeyError(f'Invalid keys: {keys_all} (error at key {key})')
+        
+        if not ((isinstance(descriptions, Sequence) and isinstance(descriptions[0], str))):
+            raise RuntimeError(f'Did not get expected format for descriptions: {descriptions} (keys: {keys_all})')
+        
+        return descriptions
+    
+    def get_datatype_suffix_from_description(self, description: str):
+        description = description.strip()
+        for datatype in self.datatypes:
+            if datatype == DATATYPE_ANAT:
+                for suffix in self.suffixes_anat:
+                    if description in self.get_descriptions([datatype, suffix]):
+                        return datatype, suffix
+            else:
+                if description in self.get_descriptions([datatype]):
+                    return datatype, None
