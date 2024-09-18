5329950235225482f526fab79c8ccac289851be8
Michelle Wang <tomichellewang@gmail.com>
2024-09-18 14:20:29 -0400

    delete files related to Python package

--- nipoppy_cli/nipoppy/tabular/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Classes for tabular data representation/manipulation."""
-
-from .bagel import Bagel
-from .doughnut import Doughnut, generate_doughnut, update_doughnut
-from .manifest import Manifest
